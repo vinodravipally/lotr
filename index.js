efe181810934e7d6ab9b237f251bc0b5a6f3339a
vinodravipally <vinod.reddy.r@gmail.com>
2022-12-13 18:31:27 -0800

    using a different http request mechanism

--- index.js
+++ index.js
@@ -4,18 +4,41 @@ const lotr = (string) => {
 };
 
 const lotrBooks = () => {
+  // var result;
+  // fetch("https://the-one-api.dev/v2/book", {
+  //   method: "GET",
+  //   headers: {
+  //     "Authorization": "Bearer AxoX8sb7aJLhtwQd1Nbn",
+  //   },
+  // })
+  //   .then((response) => response.json())
+  //   .then(function(json){
+  //     result = json;
+  //   });
+  //   return json;
   var result;
-  fetch("https://the-one-api.dev/v2/book", {
-    method: "GET",
-    headers: {
-      "Authorization": "Bearer AxoX8sb7aJLhtwQd1Nbn",
-    },
-  })
-    .then((response) => response.json())
-    .then(function(json){
-      result = json;
-    });
-    return json;
+  const xhr = new XMLHttpRequest()
+  xhr.open("GET", "https://the-one-api.dev/v2/book")
+  xhr.setRequestHeader("Authorization", "Bearer AxoX8sb7aJLhtwQd1Nbn");
+  //send the Http request
+  xhr.send()
+
+  //EVENT HANDLERS
+
+  //triggered when the response is completed
+  xhr.onload = function() {
+    if (xhr.status === 200) {
+      data = JSON.parse(xhr.responseText)
+      result = data;
+    } else if (xhr.status === 404) {
+      console.log("No records found")
+    }
+  }
+
+  xhr.onerror = function() {
+    console.log("Network error occurred")
+  }
+  return result;
 };
 
 exports.lotr = lotr;
