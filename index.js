b3b97ad1272ba0aedb5d505ff52b2d711a294634
vinodravipally <vinod.reddy.r@gmail.com>
2022-12-13 18:03:00 -0800

    updated the index.js file

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
-module.exports = function lotr(string) {
+exports.lotr = function(string) {
   if (typeof string !== "string") throw new TypeError("lotr wants a string!");
   return string.replace(/\s/g, "");
+};
+
+exports.getAllLotrBooks = function(){
+  var result;
+  fetch("https://the-one-api.dev/v2/book", {
+    method: "GET",
+    headers: {
+      "Authorization": "Bearer AxoX8sb7aJLhtwQd1Nbn",
+    },
+  })
+    .then((response) => response.json())
+    .then(function(json){
+      result = json;
+    });
+    return json;
 };
